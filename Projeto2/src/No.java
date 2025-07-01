0dec3e3ea4513c59031ec97c7ce8228a049df7cb
Marcelo Samuel Martins Lallo Filho <160680124+Lall0@users.noreply.github.com>
2025-07-01 13:20:50 -0300

    Add files via upload

---
+++ Projeto2/src/No.java
@@ -0,0 +1,8 @@
+public class No {
+    Palavra palavra;
+    No esquerda, direita;
+
+    No(Palavra palavra) {
+        this.palavra = palavra;
+    }
+}
---
+++ Projeto2/src/Palavra.java
@@ -0,0 +1,9 @@
+public class Palavra {
+    String texto;
+    int ocorrencias;
+
+    Palavra(String texto) {
+        this.texto = texto;
+        this.ocorrencias = 1;
+    }
+}
